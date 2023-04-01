bfae2b4511210e0355079dcda98a8671343160a9
Himansu Ranjan Patra <mzzhimansu@gmail.com>
2023-04-01 12:22:02 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.linkedin.com/in/himansu-ranjan-patra-6540b8202/" target={"blank"}>LinkedIn</a>
+            <a href="https://www.instagram.com/ranjan_himansu_/" target={"blank"}>Instagram</a>
+            <a href="https://twitter.com/HimansuRanjan17" target={"blank"}>Twitter</a>
+            <a href="https://github.com/HimansuRanjan" target={"blank"}>Github</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
